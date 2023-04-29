410d2620c0dbd4d8eab9e2c683183f69ea92affe
oguzhan <oguzhanselamoglu@outlook.com>
2023-04-29 11:49:57 +0300

    added capacitor network

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,7 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, NgZone, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PluginListenerHandle } from '@capacitor/core';
+import { ConnectionStatus, Network } from '@capacitor/network';
 
 @Component({
   selector: 'app-folder',
@@ -9,9 +11,31 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
+  private ngZone = inject(NgZone);
+  networkListener!: PluginListenerHandle;
+  status!: boolean
   constructor() {}
 
-  ngOnInit() {
+  async ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+
+    this.networkListener = await Network.addListener('networkStatusChange', status => {
+      console.log('Network status changed', status);
+      this.ngZone.run(()=> {
+        this.changeStatus(status);
+      })
+    });
+
+
+      const status = await Network.getStatus();
+      console.log('Network status:', status);
+      this.changeStatus(status);
+      console.log('Network status:', this.status);
+
   }
+
+  changeStatus(status: ConnectionStatus) : void{
+    this.status = status?.connected;
+  }
+
 }
